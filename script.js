7952fd1f8f93327502b637408824020343aaf6d5
ImOpNis <160320546+ImOpNis@users.noreply.github.com>
2024-07-11 14:52:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const text1Input = document.getElementById("text1");
+const text2Input = document.getElementById("text2");
+const resultElement = document.getElementById("result");
+const calculateButton = document.getElementById("calculateButton");
+const p = document.querySelector(".container p");
+
+
+let calculateSimilarity = (text1, text2) => {
+const tokens1 = text1.split(" ");
+const tokens2 = text2.split(" ");
+
+const set1 = new Set(tokens1);
+const set2 = new Set(tokens2);
+
+const intersection = new Set([...set1].filter((x) => set2.has(x)));
+
+const union = new Set([...set1, ...set2]);
+
+const similarity = (intersection.size / union.size) * 100;
+
+return similarity;
+};
+
+calculateButton.addEventListener("click", () => {
+    const text1 = text1Input.value;
+    const text2 = text2Input.value;
+
+    const similarity = calculateSimilarity(text1, text2);
+    p.style.display = '';
+
+    resultElement.textContent = similarity.toFixed(2);
+});
